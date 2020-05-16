24b9cfa1073c16e3585c78b2a539a95184aad8f6
dpopadic <dario.popadic@yahoo.com>
2020-05-16 14:23:22 +0100

    pkg structure setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='charut',
+      version='0.1',
+      description='Charting Utilities',
+      url='git@github.com:dpopadic/charut.git',
+      author='Dario Popadic',
+      author_email='dario.popadic@yahoo.com',
+      license='MIT',
+      packages=['charut'],
+      zip_safe=False)

e7748c2d3269a75234deed808bb93eb4c3e41feb
dpopadic <dario.popadic@yahoo.com>
2020-05-17 13:34:13 +0100

    default parameters fix

--- charut/charting.py
+++ charut/charting.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plt_tsline(data, title, subtitle, var_names=None):
+def plt_tsline(data, title=None, subtitle=None, var_names=None):
     """
     Plots a time series chart.
 
@@ -11,9 +11,10 @@ def plt_tsline(data, title, subtitle, var_names=None):
     data:       pandas dataframe
                 a dataframe with columns date, val and optional column field if more
                 than one series should be drawn
-    title:      str
+    title:      str, default None
                 a title for the chart
-    subtitle:   a subtitle for the chart
+    subtitle:   str, default None
+                a subtitle for the chart
     var_names:  dict
                 optional, if colnames in data are not date, val & field, declare them
                 here (eg. {'dates':'date', 'cum_val':'val'})
@@ -35,11 +36,12 @@ def plt_tsline(data, title, subtitle, var_names=None):
     data = data.set_index("date")
     fig = plt.figure()
     fig = data["val"].plot(kind="line")
-    fig = plt.suptitle(title, x=0.1, y=0.95, horizontalalignment='left', verticalalignment='top')
-    fig = plt.title(subtitle, y=1, fontsize=7, loc="left")
+    if title is not None:
+        fig = plt.suptitle(title, x=0.1, y=0.95, horizontalalignment='left', verticalalignment='top')
+    if subtitle is not None:
+        fig = plt.title(subtitle, y=1, fontsize=7, loc="left")
+
     return fig
-
-
 
 
 
